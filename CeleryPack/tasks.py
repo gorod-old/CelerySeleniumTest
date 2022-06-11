3541bc52f385229e6ac99ce1875522035afd7c94
Alex Gorodetsky <gorod1974@hotmail.com>
2022-06-11 11:49:50 +0300

    commit 10.06.2022

--- CeleryPack/tasks.py
+++ CeleryPack/tasks.py
@@ -1,7 +1,20 @@
-from CeleryPack.celery import app
+from celery import Celery
+from celery.schedules import crontab
+
+from CeleryPack import celeryconfig
 from main import check_table_data
 
+app = Celery('tasks')
 
-@app.task(queue='default', routing_key='default')
+app.config_from_object(celeryconfig, namespace='CELERY')
+
+app.conf.beat_schedule = {
+    'run_channels_pars_msgs': {
+        'task': 'CeleryPack.tasks.run_main',
+        'schedule': crontab(minute='*/10'),
+    }
+}
+
+@app.task
 def run_main():
     check_table_data()
